4c4e4f5b1561418a3fb47fbd5d182822bee7e02f
Benjamin Huy Pham <benjaminhpham@gmail.com>
2023-08-24 14:37:56 -0700

    setup errorMiddleware handler

--- server/server.js
+++ server/server.js
@@ -3,12 +3,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
 app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 8000, () =>
   console.log(`Server is running on PORT ${PORT}`)
